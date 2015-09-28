f19f738c616aeb6990d8643ddee08082fbd02aff
Joe <bioshazard@users.noreply.github.com>
2015-09-28 19:25:03 -0400

    track where we send ppl

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php redirect302($_app['ini']);
+<?php 
+
+$info = array(
+	'link' => $_app['ini'],
+	'http_referer' => $_SERVER['HTTP_REFERER'],
+	'remote_addr' => $_SERVER['REMOTE_ADDR']
+);
+
+(new ExternalLinks)->insertArray($info);
+
+redirect302($info['link']);
